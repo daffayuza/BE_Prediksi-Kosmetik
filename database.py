04ef5ecebdba6e31f4507d77c96744dba22d6ad7
DaffaYuza <muhammaddaffayuza@gmail.com>
2025-11-12 16:22:41 +0700

    update URL databse

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:@localhost/prediksi_kosmetik"
+DATABASE_URL = "mysql+mysqlconnector://root:@localhost/kosmetik_improve"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
